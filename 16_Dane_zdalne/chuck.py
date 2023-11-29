0fe746a3c980cbd26efb9b64b82ea219b5a9eff8
punio24 <punio24@hotmail.com>
2023-11-29 13:38:07 +0100

    szkolenie

--- 16_Dane_zdalne/chuck.py
+++ 16_Dane_zdalne/chuck.py
@@ -1,3 +1,7 @@
+# doinstalowac w terminalu pip install requests
+# lub dodac z plugin
+
+
 import requests
 from pprint import pprint
 
@@ -10,3 +14,16 @@ print()
 print(25*"*")
 print()
 print(response.json()['value'])
+
+import requests
+from pprint import pprint
+
+url = "https://restcountries.com/v3.1/name/Poland?fullText=true"
+response = requests.request(method="GET", url=url)
+
+pprint(response.json())
+
+print()
+print(25*"*")
+print()
+print(response.json()[0]['currencies'])
